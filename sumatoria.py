14470cf0fc1b2ae5850a8a4c771f0dac637bda87
LuisVenegas <@luvenegas>
2020-09-23 19:19:21 -0600

    se agrega sumatoria cubica

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,17 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+	x=0
+	for i in range(1,n):
+		x=x+1
+		for j in range(1,i):
+			x=x+1
+			for k in range(j,i+j):
+				x=x+1
+	print(x)
+	return x
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+	x=0
+	for i in range(1,n):
+		x=x+1
 
+#sumatoria_cubica(5)
