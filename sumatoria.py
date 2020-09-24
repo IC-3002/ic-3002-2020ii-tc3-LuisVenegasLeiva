d5869b1cb3744bc655324735a7d3887149ada612
LuisVenegas <@luvenegas>
2020-09-23 21:34:33 -0600

    se agrega sumatoria lineal

--- sumatoria.py
+++ sumatoria.py
@@ -1,17 +1,24 @@
+#############################################
+#Calculo de sumatoria		    			#
+#Luis Venegas Leiva 2019079322	      	    #
+#Ultima modificacion: 23/09/2020 21:00	    #
+#############################################
+
 def sumatoria_cubica(n):
 	x=0
-	for i in range(1,n):
-		x=x+1
+	for i in range(0,n+2):
 		for j in range(1,i):
-			x=x+1
 			for k in range(j,i+j):
 				x=x+1
-	print(x)
+	#print(x)
 	return x
-
 def sumatoria_constante(n):
 	x=0
-	for i in range(1,n):
-		x=x+1
-
-#sumatoria_cubica(5)
+	n=n-1
+	x=(n*n*n+6*n*n+11*n+6)/3
+	#print(x)
+	return x
+	
+#n=3
+#sumatoria_cubica(n)
+#sumatoria_constante(n)
